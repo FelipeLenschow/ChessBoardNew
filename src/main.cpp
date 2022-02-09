8284ed544a9806921d28d6dfe89bacf6e21ffec9
Felipe <felip@DESKTOP-IDSEDKR>
2022-02-09 10:39:57 -0300

    Better read - capacitive sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+// #include<Arduino.h>
 //#include <Print.h>
 //#include <HardwareSerial.h>
 #define F_CPU 16000000UL
@@ -8,7 +8,7 @@
 
 // Valores para calibrar sensibilidade
 #define triggerValue 6     // Se a variação entre a media e a ultima leitura não passar desse valor, o sensor não é considerado ativo
-#define divisionValue 8.00 // Define de quantos valores a variavel media vai calcular o valor
+#define divisionValue 5.00 // Define de quantos valores a variavel media vai calcular o valor
 #define sumValue 0         // Valor empirico para fazer a calibragem mais rapida
 
 // Funções basicas
@@ -135,7 +135,7 @@ void trigger()
       Rselected = i;                   // Salvando o resultado
     }
 }
-void Print()
+/*void Print()
 {
   for (int i = 63; i < 64; i++)
   {
@@ -150,35 +150,7 @@ void Print()
     // Serial.print(" ");
   }
   Serial.println("");
-}
-
-long ReadCapacitiveSensor(uint8_t samples, uint8_t MUX)
-{
-  total = 0;                            // Zera a variavel que salva o resultado
-  for (uint8_t i = 0; i < samples; i++) // Faz "sample" leituras, somando tudo em total
-  {                                     // Faz a leitura do sensor
-    set_bit(DDRD, receivePin[MUX]);     // Receive OUTPUT
-    clr_bit(PORTD, receivePin[MUX]);    // Receive LOW
-    _delay_us(10);                      // Espera 10ms
-    clr_bit(DDRD, receivePin[MUX]);     // Receive INPUT
-    clr_bit(PORTD, receivePin[MUX]);    // Receive PULLUP desligados
-    set_bit(PORTD, sendPin);            // Send HIGH
-
-    while (!tst_bit(PIND, receivePin[MUX])) // Enquanto Receive está LOW
-      total++;
-
-    set_bit(DDRD, receivePin[MUX]);  // Receive OUTPUT
-    set_bit(PORTD, receivePin[MUX]); // Receive HIGH
-    //_delay_us(10);                   // Espera 10ms
-    clr_bit(DDRD, receivePin[MUX]);  // Receive INPUT
-    clr_bit(PORTD, receivePin[MUX]); // Receive PULLUP desligados
-    clr_bit(PORTD, sendPin);         // Send LOW
-
-    while (tst_bit(PIND, receivePin[MUX])) // Enquanto Receive está HIGH
-      total++;
-  }
-  return (total); // retorna o valor
-}
+}*/
 void ReadAll()
 {
   for (uint8_t casa = 0; casa < 64; casa++)
@@ -196,11 +168,11 @@ void ReadAll()
     MUX = casa / 16; // Seleciona a casa co mux
     total = 0;       // Zera a variavel que salva o resultado
 
-    for (uint8_t i = 0; i < 10; i++)    // Faz "sample" leituras, somando tudo em total
+    for (uint8_t i = 0; i < 10; i++)   // Faz "sample" leituras, somando tudo em total
     {                                  // Faz a leitura do sensor
       set_bit(DDRD, receivePin[MUX]);  // Receive OUTPUT
       clr_bit(PORTD, receivePin[MUX]); // Receive LOW
-      _delay_us(10);                   // Espera 10ms
+      _delay_us(3);                    // Espera 10ms
       clr_bit(DDRD, receivePin[MUX]);  // Receive INPUT
       clr_bit(PORTD, receivePin[MUX]); // Receive PULLUP desligados
       set_bit(PORTD, sendPin);         // Send HIGH
@@ -210,7 +182,7 @@ void ReadAll()
 
       set_bit(DDRD, receivePin[MUX]);  // Receive OUTPUT
       set_bit(PORTD, receivePin[MUX]); // Receive HIGH
-      _delay_us(10);                   // Espera 10ms
+      _delay_us(3);                    // Espera 10ms
       clr_bit(DDRD, receivePin[MUX]);  // Receive INPUT
       clr_bit(PORTD, receivePin[MUX]); // Receive PULLUP desligados
       clr_bit(PORTD, sendPin);         // Send LOW
@@ -220,7 +192,7 @@ void ReadAll()
     }
 
     // Faz a leitura e salva uma media dos ultimos valores
-    value[casa] += (total - value[casa]) / 1.50;          // Somando parte da leitura atual na variavel que guarda o valor, para deixar a curva mais suave
+    value[casa] += (total - value[casa]) / 1.50;                // Somando parte da leitura atual na variavel que guarda o valor, para deixar a curva mais suave
     media[casa] += (value[casa] - media[casa]) / divisionValue; // Media dos ultimos valores lidos, para deixar as leituras completamente lineares
   }
   trigger(); // Testa para ver se algum sensor pode ser considerado ativado
@@ -230,33 +202,6 @@ void ReadAll()
       if (casa != Rselected)
         media[casa] += (value[casa] - media[casa]) / divisionValue; // Media dos ultimos valores lidos, para deixar as leituras completamente lineares
   */
-}
-void Calibragem()
-{
-  for (uint8_t i = 0; i < 25; i++)
-  {
-    for (uint8_t casa = 0; casa < 64; casa++)
-    {
-      //  Liga o MUX de tal casa
-      if (!tst_bit(casa, 5) && !tst_bit(casa, 4))     // "Abrindo" binario, 00 liga a porta 4
-        PORTC = 0b11100000 + casa % 16;               // Salvando no PORTC a casa escolhida
-      else if (!tst_bit(casa, 5) && tst_bit(casa, 4)) // "Abrindo" binario, 01 liga a porta 5
-        PORTC = 0b11010000 + casa % 16;               // Salvando no PORTC a casa escolhida
-      else if (tst_bit(casa, 5) && !tst_bit(casa, 4)) // "Abrindo" binario, 10 liga a porta 6
-        PORTC = 0b10110000 + casa % 16;               // Salvando no PORTC a casa escolhida
-      else if (tst_bit(casa, 5) && tst_bit(casa, 4))  // "Abrindo" binario, 11 liga a porta 7
-        PORTC = 0b01110000 + casa % 16;               // Salvando no PORTC a casa escolhida
-      // PORTC = casa % 16;
-
-      // Salva a ultima leitura para ser usada como valor inicial
-      value[casa] = ReadCapacitiveSensor(10, casa / 16); // Pedindo 10 leituras do MUX casa/16 - (0, 1, 2 ou 3)
-      media[casa] = (value[casa]) + sumValue;            // A media é coletada da ultima leitura e somada de 4 para evitar erro no inicio que ainda é instavel
-      // Print();
-    }
-  }
-  for (int i = 0; i < 5; i++)
-    ReadAll();
-  // SerialWrite('c');
 }
 
 // Funções para identificar movimentação
@@ -277,7 +222,7 @@ void getMove()
   LedShow();                                     // Grava a cor no led
   start = Rselected;                             // Para evitar de uma peça ser movida para a casa que ja está
 
-  for (int i = 0; i < 15; i++)                   // Calibragem rapida
+  for (int i = 0; i < 10; i++)                   // Calibragem rapida
     ReadAll();                                   // Le os sensores
   while (Rselected == 100 || Rselected == start) // Espera até que seja lido uma casa
     ReadAll();                                   // Le os sensores
@@ -300,14 +245,14 @@ void gotMoved()
   fCasa = (Rmove[3] - 49) * 8 + Rmove[2] - 97;
 
   // Confere se a casa recebida foi movida
-  for (int i = 0; i < 15; i++) // Calibragem rapida
+  for (int i = 0; i < 10; i++) // Calibragem rapida
     ReadAll();                 // Le os sensores
   while (Rselected != sCasa)   // Espera até que seja lido uma casa
     ReadAll();                 // Le os sensores
   corCasa(sCasa, 0, 0, 0);     // Acende o led da casa
   LedShow();                   // Grava a cor no led
 
-  for (int i = 0; i < 15; i++) // Calibragem rapida
+  for (int i = 0; i < 10; i++) // Calibragem rapida
     ReadAll();                 // Le os sensores
   while (Rselected != fCasa)   // Espera até que seja lido uma casa
     ReadAll();                 // Le os sensores
@@ -346,21 +291,17 @@ int main(void)
   DDRD = 0b10000000;  // send output, receives input
   PORTD = 0b00000000; // send pin low
 
-  SerialBegin(9600); // Inicia o Serial, com baud de 9600
-  LedShow();         // Reseta os leds
-  Calibragem();      // Calibra a variavel media e value
+  SerialBegin(9600);           // Inicia o Serial, com baud de 9600
+  LedShow();                   // Reseta os leds
+  for (int i = 0; i < 30; i++) // Calibra a variavel media e value
+    ReadAll();
 
   while (true)
   {
-    //Sempre();
+    // Sempre();
     Data = SerialReadLock();
-    if (Data == 'N' && SerialReadLock() == 'w')
-    {                                 // Caso o player seja brancas
-      getMove();                      // Coleta o movimento
-      for (uint8_t i = 0; i < 4; i++) // Envia o movimento usando Serial
-        SerialWrite(Smove[i]);
-    }
-    else if (Data == 'M')
+
+    if (Data == 'M')
     {                                // Recebendo um movimento executado pela engine no PC
       for (int i = 0; i < 4; i++)    // Le 4 vezes
         Rmove[i] = SerialReadLock(); // O movimento chegando
@@ -376,6 +317,12 @@ int main(void)
 
       for (uint8_t i = 0; i < 4; i++) // Repete por 4 vezes
         SerialWrite(Smove[i]);        // O envio dos 4 char usando Serial
+    }
+    else if (Data == 'N' && SerialReadLock() == 'w')
+    {                                 // Caso o player seja brancas
+      getMove();                      // Coleta o movimento
+      for (uint8_t i = 0; i < 4; i++) // Envia o movimento usando Serial
+        SerialWrite(Smove[i]);
     }
     else if (Data == 'E')
     {
